9a8681f7619037e8dafc0e49889ef7615fca0dff
Michael Bade <mbade314@gmail.com>
2020-05-12 18:36:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,16 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0
+  while i < array.length
+    if array[i].length > 4
+      true
+    else
+      false
+    end
+    i += 1
+  end
 end
 
 def find_the_cheese# code an argument here
