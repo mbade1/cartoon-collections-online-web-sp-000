cf87d9c1c18b8235da3dfc5572289f4ced65d336
Michael Bade <mbade314@gmail.com>
2020-05-12 18:39:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,13 +14,13 @@ end
 
 def long_planeteer_calls(array)
   i = 0
-    if array[i].length >= 4
-      return true
-    else
-      return false
-    end
-    i += 1
+  if array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i += 1
   end
+end
 
 
 def find_the_cheese# code an argument here
